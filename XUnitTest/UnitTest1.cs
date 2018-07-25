ff32712194f52d4e3e765ece150d90b95cc13f7c
rodericklgp <roderick_lgp@qq.com>
2018-07-25 15:47:21 +0800

    添加项目文件。

---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact(DisplayName = "显示为通过测试")]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+        [Fact(Skip = "显示为跳过测试")]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+        [Fact(DisplayName = "显示为跳过测试")]
+        public void FailingTest1()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        [Theory]
+        [InlineData(1, 2)]
+        public void TheoryPassingTest(int a, int b)
+        {
+            Assert.Equal(3, Add(a, b));
+        }
+        [Theory]
+        [InlineData(3, 4)]
+        public void TheoryPassingTest2(int a, int b)
+        {
+            Assert.Equal(3, Add(a, b));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
